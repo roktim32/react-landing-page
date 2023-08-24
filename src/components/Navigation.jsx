f0da9db65a63c2d40a9f7c68a961c517f44ede04
Roktim Kamal Senapoty <roktimsenapoty2@gmail.com>
2023-08-24 20:20:27 +0530

    basic landing page

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,28 @@
+import "../App.css";
+
+const Navigation = () => {
+  return (
+    <nav className="container">
+      <div className="logo">
+        <img src="/images/brand_logo.png" alt="" />
+      </div>
+      <ul>
+        <li>
+          <a href="#">Menu</a>
+        </li>
+        <li>
+          <a href="#">Location</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+      <button>Get Started</button>
+    </nav>
+  );
+};
+
+export default Navigation;
